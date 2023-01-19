a08c5d891319cd95727db263f76272a3ca41a38e
Miguel Jimenez <chamuel.developer@gmail.com>
2023-01-18 20:34:04 -0500

    Initial Commit

---
+++ Acronyms App/Acronyms_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Acronyms_AppApp.swift
+//  Acronyms App
+//
+//  Created by chamuel castillo on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct Acronyms_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
